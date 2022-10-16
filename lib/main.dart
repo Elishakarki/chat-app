0aa19615f35dd77274c2c34417755ddd237426d2
Elisha Karki <karkielisha222@gmail.com>
2022-10-16 10:28:37 +0545

    Navigating with routes

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:prepareapp/screens/welcome_screen.dart';
+import 'package:prepareapp/screens/login_screen.dart';
+import 'package:prepareapp/screens/registration_screen.dart';
+import 'package:prepareapp/screens/chat_screen.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    
+    return MaterialApp(
+       theme: ThemeData.dark().copyWith(
+        textTheme: const  TextTheme(
+          bodyText1: TextStyle(color: Colors.black54),
+        ),
+      ),
+      
+       initialRoute: '/',
+  routes: {
+    // When navigating to the "/" route, build the FirstScreen widget.
+    '/': (context) =>  WelcomeScreen(),
+    // When navigating to the "/second" route, build the SecondScreen widget.
+    '/second': (context) => RegistrationScreen(),
+    '/third':(context) => LoginScreen(),
+    'fourth':(context) => ChatScreen(),
+  },
+);
+    
+  }
+}
